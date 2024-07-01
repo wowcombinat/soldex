4b24bc9dce8806c64411deb2210a07dd5048260e
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-01 18:32:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 import './index.css';
 import App from './App';
 
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const wallets = [new PhantomWalletAdapter()];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
