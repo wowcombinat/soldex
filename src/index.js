e365ffb0a3a39e3d0ccda93ac84d69b666b70198
wowcombinat <48298307+wowcombinat@users.noreply.github.com>
2024-07-01 18:49:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 import './index.css';
 import App from './App';
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie11';
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
 
 require('@solana/wallet-adapter-react-ui/styles.css');
 
